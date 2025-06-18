8d258f8540485200fd6da98d0fd983f16ec29cf6
muralikrishnasoma33 <muralikrishnasoma33@gmail.com>
2025-06-18 18:07:31 +0530

    Added to

--- First.java
+++ First.java
@@ -1,7 +1,7 @@
 class First
 {
-    public static void main(string[] args)
+    public static void main(String[] args)
     {
-        System.out.println("Welcome the World");
+        System.out.println("Welcome to the World");
     }
 }
