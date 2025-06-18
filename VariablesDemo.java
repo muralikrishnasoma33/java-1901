03b4f5ee5f0ed9da6b95c3e1c6eb1a6b4eb1334d
muralikrishnasoma33 <muralikrishnasoma33@gmail.com>
2025-06-18 22:09:43 +0530

    VariablesDemo.java

---
+++ VariablesDemo.java
@@ -0,0 +1,12 @@
+public class VariablesDemo {
+    public static void main(String[] args)
+    {
+        int age = 23;
+        String name = "Murali Krishna";
+        String message = "Welcome to the Corejava class";
+        System.out.println(age);
+        System.out.println(name);
+        System.out.println(message);
+    }
+    
+}
