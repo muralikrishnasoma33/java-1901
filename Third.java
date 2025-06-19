54fde80cdb8f7c5067d9c908bc310946a08e5a28
muralikrishnasoma33 <muralikrishnasoma33@gmail.com>
2025-06-19 12:39:15 +0530

    DataTypes.java

---
+++ Third.java
@@ -0,0 +1,25 @@
+public class Datatypes {
+    public static void main(String[] args)
+    {
+        String name = "Soma Murali Krishna";
+        byte age = 23;
+        String email = "muralikrishnasoma99@gmail.com";
+        float height = 5.7f;
+        short weight = 69;
+        int salary = 25000;
+        double percentage = 73.5;
+        char grade = 'C';
+        String college = "Vaagdevi Institute of Technology and Science";
+
+        System.out.println(name);
+        System.out.println(age);
+        System.out.println(email);
+        System.out.println(height);
+        System.out.println(weight);
+        System.out.println(salary);
+        System.out.println(percentage);
+        System.out.println(grade);
+        System.out.println(college);
+    }
+    
+}
