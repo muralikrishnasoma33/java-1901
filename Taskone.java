7ad0987a6db15f24309294317e7b6c0024d589bf
muralikrishnasoma33 <muralikrishnasoma33@gmail.com>
2025-06-19 21:12:56 +0530

    First Task

---
+++ Taskone.java
@@ -0,0 +1,21 @@
+public class Taskone {
+    public static void main(String[] args)
+    {
+        String brandName = "Highlander";
+        String product = "Men Comfort Relaxed Fit Textured Spread Collar Casual Shirt";
+        float rating = 4.6f;
+        int actualPrice = 3949;
+        double off = 81;
+        int finalPrice = 750;
+        int size = 39;
+
+        System.out.println("BrandName    : " +brandName);
+        System.out.println("Product      : " +product);
+        System.out.println("Rating       : " +rating);
+        System.out.println("ActualPrice  : " +actualPrice);
+        System.out.println("off %        : "+off);
+        System.out.println("FinalPrice   : "+finalPrice);
+        System.out.println("Size         : "+size);
+    }
+    
+}
--- Third.java
+++ Third.java
@@ -1,4 +1,4 @@
-public class Datatypes {
+public class Third {
     public static void main(String[] args)
     {
         String name = "Soma Murali Krishna";
