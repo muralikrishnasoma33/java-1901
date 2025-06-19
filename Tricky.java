f5d351fabd2f944e4bf9d2d313bf3f8f95639bb5
muralikrishnasoma33 <muralikrishnasoma33@gmail.com>
2025-06-19 15:07:01 +0530

    fourth program

---
+++ Tricky.java
@@ -0,0 +1,14 @@
+public class Tricky {
+    public static void main(String[] args) {
+        int x = 10;
+        //By changing of increment and decrement can we get same values or different values of
+        //x and y........
+
+        int y = x++ + ++x;
+        System.out.println("x : " +x);
+        System.out.println("y : " +y);
+
+        //Guess x and y value
+        
+    }
+}
