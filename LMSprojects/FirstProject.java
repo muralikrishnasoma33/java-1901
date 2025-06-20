46c8301d20ac95b5afab7b40d6c5406e10a3661e
muralikrishnasoma33 <muralikrishnasoma33@gmail.com>
2025-06-20 18:20:07 +0530

    Variables and Datatypes.PROJECT

--- LMSprojects/FirstProject.java
+++ LMSprojects/FirstProject.java
@@ -1,7 +1,27 @@
 package LMSprojects;
 
-public class FirstProject {
-    
+public class FirstProject 
+{
+    public static void main(String[] args)
+    {
+       int studentID = 201;
+       String studentName = "Murali Krishna";
+       int studentAge = 23;
+       char studentGender = 'M';
+       float studentRating = 4.5f;
+       boolean isInstructor = false;
+       String courseName = "Java";
+       short courseID = 301;
 
-    
+
+       System.out.println("ID          : "+studentID);
+       System.out.println("Name        : "+studentName);
+       System.out.println("Age         : "+studentAge);
+       System.out.println("Gender      : "+studentGender);
+       System.out.println("Rating      : "+studentRating);
+       System.out.println("Is Trainer  : "+isInstructor);
+       System.out.println("Course name : "+courseName);
+       System.out.println("Course ID   : "+courseID);
+    }
 }
+
