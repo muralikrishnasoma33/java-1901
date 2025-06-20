126e27633804d0283642b9fd1e74f4299152b52e
muralikrishnasoma33 <muralikrishnasoma33@gmail.com>
2025-06-20 22:54:32 +0530

    Type Casting and Type Conversion -Rules -Definitions -Program

---
+++ LMSprojectOne/TypeCC.java
@@ -0,0 +1,48 @@
+package LMSprojectOne;
+
+public class TypeCC {
+    public static void main(String[] args) {
+        //Type Conversion
+        int num = 10; //4bytes
+        double number = num; // 8bytes
+        System.out.println(number);
+
+        short m = 123;
+        float n = m;
+        System.out.println(n);
+
+        //Type Casting
+        double value = 13.3; //8bytes
+        int newvalue = (int) value; //4bytes
+        System.out.println(newvalue);
+
+        //if conversion not possible directly
+        int data = 200;
+        String newdata = String.valueOf (data);
+        System.out.println(newdata);
+
+        System.out.println(data+100);
+        System.out.println(newdata+200);
+
+        //Conversion Not Possible at all
+        int j = 89;
+        //boolean newj = (int) j;
+    }  
+}
+        //Type Conversion & Type Casting
+
+        // -> Type Conversion: 
+        //    Converting from one datatype to another datatype AUTOMATICALLY 
+        //    by compiler.
+        //    - No data loss
+        //    - Small to Big (Widening)
+        //    - Handled by java compiler
+
+
+        //-> Type Casting : 
+        //   Converting from one datatype to another datatype MANUALLY/EXPLICITLY 
+        //   - Data loss may occur.
+        //   - Big to Small(Narrowing)
+        //   - Handled by Developer
+        //        -specify the datatype(paranthesis/data).
+
