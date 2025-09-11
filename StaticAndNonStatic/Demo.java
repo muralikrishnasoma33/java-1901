3776db418bdd5c64f1c8d1a88117b4f6eb95b201
muralikrishnasoma33 <muralikrishnasoma33@gmail.com>
2025-09-11 11:19:48 +0530

    Purpose Of Static

---
+++ StaticAndNonStatic/Demo.java
@@ -0,0 +1,15 @@
+package StaticAndNonStatic;
+
+public class Demo {
+    
+    
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 20;
+
+        System.out.println((a+b));
+    }
+
+       //Here we are not creating an object...
+
+}
